c6d341c95b9e00c04da10c04458c1b4792912418
Branden Gibson <seishunomutsu@gmail.com>
2017-11-14 02:55:02 +0000

    Added user profile creation form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
     # GET to /users/:user_id/profile/new
     def new
         # Render blank profile details from 
+        @profile = Profile.new
     end
 end
